f0057bef541c20cad79282967d21f6f2d58a0029
joe fisher <jhanajon@gmail.com>
2018-07-07 15:53:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,15 +15,3 @@ function happyHolidayTo(holiday, name) {
 function holidayCountdown(days, holiday) {
   return `It's ${days} days until ${holiday}!`
 }
-
-///added a reverse in string options but modified index-test.js to reflect the expectation:
-//expect(holidayCountdown(20, "Mother's Day"))
-//in lieu of
-//expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
-
-// then I split up into separate functions in the test file which did not work and I don't understand it
-
-
-// function holidayCountdownReverse(days, holiday) {
-//   return `It's ${days} days until ${holiday}!`
-// }
