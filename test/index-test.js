cc16b86594815c0c66334d54ebac7b397186c5be
joe fisher <jhanajon@gmail.com>
2018-07-07 15:35:42 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -25,12 +25,10 @@ describe('functions', () => {
       expect(holidayCountdown(20, "Mother's Day")).toEqual("It's 20 days until Mother's Day!")
     })
   })
-})
 
 describe('holidayCountdownReverse(holiday, days)', () => {
   it('returns "It\'s ${days} days until ${holiday}!"', () => {
     //expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
     expect(holidayCountdownReverse(20, "Mother's Day")).toEqual("It's 20 days until Mother's Day!")
   })
-})
 })
